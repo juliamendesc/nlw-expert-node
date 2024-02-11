54dad113dc21e0796691295b5217b4be5ba4fcc1
Julia Mendes <juliamendesc@gmail.com>
2024-02-11 23:48:00 +0000

    create docker compose, add prisma ORM, create http routes

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # nlw-expert-node
+
+A simple project to learn about Node.js, Typescript, Fastify routing management, PostgresSQL, and more.
+
+ORM - bibliotecas que trazem ferramentas para navegar no banco de dados. Usamos o Prisma.
--- src/http/server.ts
+++ src/http/server.ts
@@ -1,1 +1,20 @@
-console.log('hello');
+import cookie from '@fastify/cookie';
+import fastify from 'fastify';
+import { createPoll } from './routes/create-poll';
+import { getPoll } from './routes/get-poll';
+import { voteOnPolls } from './routes/vote-on-polls';
+
+const app = fastify();
+
+app.register(cookie, {
+	secret: 'polls-app-nlw',
+	hook: 'onRequest',
+});
+
+app.register(createPoll);
+app.register(getPoll);
+app.register(voteOnPolls);
+
+app.listen({ port: 3333 }).then(() => {
+	console.log('HTTP Server is running on port 3333');
+});
